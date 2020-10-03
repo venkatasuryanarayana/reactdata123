51747d72801a8b5fbc138084f25e8f6e2ef9fa2a
venkatasuryanarayana <venkatasuryanarayana.n@apssdc.in>
2020-10-03 18:12:56 +0530

    Updates

---
+++ precache-manifest.1b2ee6a00a7be8bcf2af707989531134.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81aad8543638de2022aa47c2d9783fe",
+    "url": "/reactdata123/index.html"
+  },
+  {
+    "revision": "8f56ddae7d495deb28c6",
+    "url": "/reactdata123/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e438f2ca2174fedcddc",
+    "url": "/reactdata123/static/js/2.2cff8d2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactdata123/static/js/2.2cff8d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f56ddae7d495deb28c6",
+    "url": "/reactdata123/static/js/main.2feae882.chunk.js"
+  },
+  {
+    "revision": "51f366cc565da9985d84",
+    "url": "/reactdata123/static/js/runtime-main.e7c09de2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdata123/static/media/logo.5d5d9eef.svg"
+  }
+]);
